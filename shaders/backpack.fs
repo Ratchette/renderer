77066b738ae3abb98867808f6eee923fc57e9dcd
Jennifer Winer <jenn.winer@gmail.com>
2023-05-30 22:35:58 +0100

    Add in a directional light

--- shaders/backpack.fs
+++ shaders/backpack.fs
@@ -1,10 +1,52 @@
 #version 330 core
-out vec4 FragColour;
 
+struct DirectionalLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 fragmentNormal;
+in vec3 fragmentPosition;
 in vec2 textureCoordinates;
 
 uniform sampler2D texture_diffuse0;
+uniform sampler2D texture_specular0;
+uniform float specular_shininess;
 
-void main() {    
-    FragColour = texture(texture_diffuse0, textureCoordinates);
+uniform DirectionalLight directionalLight;
+uniform vec3 viewerPosition;
+
+
+out vec4 FragColour;
+
+
+vec3 CalculateDirectionalLight(DirectionalLight light, vec3 normal, vec3 viewDirection);
+
+
+void main() {
+    vec3 colour = vec3(0.0);
+
+    vec3 normal = normalize(fragmentNormal);
+    vec3 viewDirection = normalize(viewerPosition - fragmentPosition);
+    
+    colour += CalculateDirectionalLight(directionalLight, normal, viewDirection);
+
+    FragColour = vec4(colour, 1.0);
+}
+
+vec3 CalculateDirectionalLight(DirectionalLight light, vec3 normal, vec3 viewDirection) {
+    vec3 lightDirection = normalize(-light.direction);
+    vec3 reflectDir = reflect(-lightDirection, normal);
+
+    float diffusionIntensity = max(dot(normal, lightDirection), 0.0);
+    float specularIntensity = pow(max(dot(viewDirection, reflectDir), 0.0), specular_shininess);
+
+    vec3 ambient    = light.ambient  * texture(texture_diffuse0,  textureCoordinates).rgb;
+    vec3 diffuse    = light.diffuse  * texture(texture_diffuse0,  textureCoordinates).rgb * diffusionIntensity;
+    vec3 specular   = light.specular * texture(texture_specular0, textureCoordinates).rgb * specularIntensity;
+
+    return ambient + diffuse + specular;
 }
--- shaders/backpack.vs
+++ shaders/backpack.vs
@@ -1,15 +1,21 @@
 #version 330 core
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
-
-out vec2 textureCoordinates;
+layout (location = 2) in vec2 aTexCoord;
 
 uniform mat4 modelTransform;
 uniform mat4 viewTransform;
 uniform mat4 perspectiveTransform;
 
+out vec3 fragmentNormal;
+out vec3 fragmentPosition;
+out vec2 textureCoordinates;
+
 void main() {
-    textureCoordinates = aTexCoords;    
-    gl_Position = perspectiveTransform * viewTransform * modelTransform * vec4(aPos, 1.0);
+	gl_Position = perspectiveTransform * viewTransform * modelTransform * vec4(aPos, 1.0f);
+	fragmentNormal = mat3(transpose(inverse(modelTransform))) * aNormal;
+	fragmentPosition = vec3(modelTransform * vec4(aPos, 1.0));
+
+	textureCoordinates = aTexCoord;
 }
